93bec5d2e4a45919e17f14770d0b0322a63f42a4
Andrew Dodson <sonofdod@gmail.com>
2016-03-01 08:12:17 +0000

    lint gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,27 +1,28 @@
+/*eslint no-console:0*/
 'use strict';
 
 let gulp = require('gulp');
 let browserify = require('browserify');
-let babelify= require('babelify');
+let babelify = require('babelify');
 let util = require('gulp-util');
 let buffer = require('vinyl-buffer');
 let source = require('vinyl-source-stream');
 let uglify = require('gulp-uglify');
 let sourcemaps = require('gulp-sourcemaps');
-let args   = require('yargs').argv;
+let args = require('yargs').argv;
 let gulpif = require('gulp-if');
 let less = require('gulp-less');
 let minifyCSS = require('gulp-minify-css');
 
 gulp.task('build', () => {
-	browserify('./src/adorn.js', { debug: true })
+	browserify('./src/adorn.js', {debug: true})
 	.transform(babelify)
 	.bundle()
 	.on('error', util.log.bind(util, 'Browserify Error'))
 	.pipe(source('./adorn.js'))
 	.pipe(buffer())
 	.pipe(sourcemaps.init({loadMaps: true}))
-	.pipe(gulpif(!args.debug, uglify({ mangle: false })))
+	.pipe(gulpif(!args.debug, uglify({mangle: false})))
 	.pipe(sourcemaps.write('./'))
 	.pipe(gulp.dest('./'))
 	.on('end', console.log.bind(console, 'BUILT'));
